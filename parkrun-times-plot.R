84b523608f0b99255998aca1b4b82202f073b520
Katie Millar <ajh20fhu@uea.ac.uk>
2023-05-08 15:13:59 +0100

    Updating plot code

--- parkrun-times-plot.R
+++ parkrun-times-plot.R
@@ -1,11 +1,20 @@
+# reading appropriate packages in 
 library(readxl)
+library(tidyverse)
 
+# reading the data in
 parkrun_times <- read_excel("parkrun-times.xlsx", sheet = "Sheet1")
 
-library(ggplot2)
 
-ggplot(parkrun_times, aes(x = date, y = time, color = parkrun)) + 
+
+# code for point plot 
+parkrun_plot <- parkrun_times %>% 
+ggplot(aes(x = date, y = time, color = parkrun)) + 
   geom_point() +
   scale_size_continuous(range = c(2, 10)) +
   labs(x = "Date", y = "Time", color = "parkrun", size = "Time") +
   theme_classic()
+
+
+
+
