ecf18b09904b1f0f45d68b918dac80c376aa165e
Katie Millar <ajh20fhu@uea.ac.uk>
2023-05-20 16:27:31 +0100

    trying to code a map of parkrun locations

---
+++ parkrun-map.R
@@ -0,0 +1,31 @@
+ggplot() + 
+  geom_polygon(data = worldmap, 
+               aes(x = long, 
+                   y = lat, 
+                   group = group)) + 
+  coord_fixed(ratio = 1.3, 
+              xlim = c(-10,3), 
+              ylim = c(50, 59))
+
+library(maps)
+
+
+worldmap = map_data('world')
+
+
+ggplot() + 
+  geom_polygon(data = worldmap, 
+               aes(x = long, y = lat, group = group), 
+               fill = 'gray90', color = 'black') + 
+  coord_fixed(ratio = 1.3, xlim = c(-10,3), ylim = c(50, 59)) + 
+  theme_void() + 
+  geom_point(data = parkrun_map, 
+             aes(x = as.numeric(lng), 
+                 y = as.numeric(lat), size = n, color = log(n)), alpha = .7) + 
+  scale_size_area(max_size = 8) + 
+  scale_color_viridis_c() + 
+  theme(legend.position = 'none') + 
+  theme(title = element_text(size = 12))
+
+
+parkrun_map <- read_excel("parkrun_map.xlsx", sheet = "Sheet1")
