83dfcc127c5ac9574bd71b45e190422ea5a1fccf
Katie Millar <ajh20fhu@uea.ac.uk>
2023-05-31 14:38:20 +0100

    making a table for parkruns

--- parkruntimes-table/app.R
+++ parkruntimes-table/app.R
@@ -2,13 +2,13 @@
 library(DT)
 
 ui <- basicPage(
-  h2("parkrun_times"),
+  h2("parkrun times"),
   DT::dataTableOutput("mytable")
 )
 
 server <- function(input, output) {
   output$mytable = DT::renderDataTable({
-    parkrun_times
+    all_parkrun
   })
 }
 
