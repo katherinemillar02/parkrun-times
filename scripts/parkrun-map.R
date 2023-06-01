d025e838280b638c7084cb14abae0954b7eca173
Katie Millar <ajh20fhu@uea.ac.uk>
2023-06-01 17:00:32 +0100

    parkun plots

--- scripts/parkrun-map.R
+++ scripts/parkrun-map.R
@@ -14,21 +14,20 @@ worldmap = map_data('world')
 
 library(tidyverse)
 
-map_plot <- ggplot() + 
-  geom_polygon(data = parkrun_map , 
-               aes(x = lng, y = lat), 
+parkrun_map_2 <- ggplot() + 
+  geom_polygon(data = worldmap, 
+               aes(x = long, y = lat, group = group), 
                fill = 'white', color = 'black') + 
   coord_fixed(ratio = 1.3, xlim = c(-10,3), ylim = c(50, 59)) + 
   theme_void() + 
   geom_point(data = parkrun_map, 
              aes(x = as.numeric(lng), 
                  y = as.numeric(lat),
-                 size = n, color = log(n)), alpha = .9) + 
+                 size = n, color = red), alpha = .9) + 
   scale_size_area(max_size = 10) + 
   scale_color_viridis_c() + 
   theme(legend.position = 'none') + 
   theme(title = element_text(size = 12))
 
-library(readxl)
 
 parkrun_map <- read_excel("data/parkrun_map.xlsx", sheet = "Sheet1")
