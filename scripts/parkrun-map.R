6e58f3361cad52c634e5d5540e9859dd5a78a9f3
Katie Millar <ajh20fhu@uea.ac.uk>
2023-06-01 16:32:30 +0100

    writing code for a parkrun map

--- scripts/parkrun-map.R
+++ scripts/parkrun-map.R
@@ -12,10 +12,11 @@ library(maps)
 
 worldmap = map_data('world')
 
+library(tidyverse)
 
-parkrun_map_2 < - ggplot() + 
-  geom_polygon(data = worldmap, 
-               aes(x = long, y = lat, group = group), 
+map_plot <- ggplot() + 
+  geom_polygon(data = parkrun_map , 
+               aes(x = lng, y = lat), 
                fill = 'white', color = 'black') + 
   coord_fixed(ratio = 1.3, xlim = c(-10,3), ylim = c(50, 59)) + 
   theme_void() + 
@@ -28,5 +29,6 @@ parkrun_map_2 < - ggplot() +
   theme(legend.position = 'none') + 
   theme(title = element_text(size = 12))
 
+library(readxl)
 
-parkrun_map <- read_excel("parkrun_map.xlsx", sheet = "Sheet1")
+parkrun_map <- read_excel("data/parkrun_map.xlsx", sheet = "Sheet1")
