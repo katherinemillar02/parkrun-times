6134b68960421cadb24d36ec4c9455b20613c528
Katie Millar <ajh20fhu@uea.ac.uk>
2023-06-01 11:11:40 +0100

    trying to create a scatter plot on shiny

--- plots/plots.R
+++ plots/plots.R
@@ -1,50 +1,25 @@
-# creating an interactive parkrun plot for a shiny app 
 
-library(plotly)
 
-plotlyOutput("plot")
+### ------------------- trying again 
+library(shiny)
 
-output$plot <- renderPlotly({
-  ggplotly(parkrun_plotly_point)
+# define server 
+shinyServer(function(input, output) {
+  
+  output$scatter <- renderPlot({
+    
+    prun <- parkrun_times[, c(input$VarX, input$VarY)]
+ plot(prun[,1], prun[,2],
+ xlab = colnames(prun) [1],
+ ylab = colnames(prun) [2],
+ main = paste("Plot of parkrun times",input$VarX, "Vs", input$VarY ),
+ col = "violet", pch = 15)
+      } )
 })
 
+ 
 
-# trying a code out 
-# website community plotly
 
-library(shiny)
-library(plotly)
 
-ui <- fluidPage(
-  headerPanel('Example'),
-  sidebarPanel(
-    selectInput('xcol','X Variable', names(parkrun_times)),
-    selectInput('ycol','Y Variable', names(parkrun_times)),
-    selected = names(parkrun_times)[[2]]),
-  mainPanel(
-    plotlyOutput('plot')
-  )
-)
 
-server <- function(input, output) {
-  
-  x <- reactive({
-    parkrun_times[,input$xcol]
-  })
-  
-  y <- reactive({
-    parkrun_times[,input$ycol]
-  })
-  
-  
-  output$plot <- renderPlotly(
-    plot1 <- plot_ly(
-      x = x(),
-      y = y(), 
-      type = 'scatter',
-      mode = 'markers')
-  )
-  
-}
 
-shinyApp(ui,server)
