807b9dc11c90616b28469242a0d3a3837514ea2f
Katie Millar <ajh20fhu@uea.ac.uk>
2023-06-01 12:01:15 +0100

    coding a scatter plot within a shiny app

--- plots/plots.R
+++ plots/plots.R
@@ -38,8 +38,66 @@ shinyServer(function(input, output) {
       } )
 })
 
- 
 
+
+library(tidyverse)
+
+
+
+ui <- fluidPage(
+  titlePanel("parkrun Plot"),
+  sidebarLayout(
+    sidebarPanel(
+      
+    ),
+    mainPanel(
+      plotOutput(outputId = "scatterplot")
+    )
+  )
+)
+
+server <- function(input, output) {
+  output$scatterplot <- renderPlot({
+    p = ggplot(data = read_excel("data/parkrun-times-2.xlsx")) +
+      aes_string(x = input$x, y = input$y) +
+      geom_point(x = date, y = time)
+    plot(p)
+    observeEvent(input$update, print(as.numeric(input$update)))
+  })
+}
+
+shinyApp(ui, server) 
+
+
+library(shiny)
+library(tidyverse)
+
+ui <- fluidPage(
+  plotOutput("plot", brush = "plot_brush", dblclick = "plot_reset")
+)
+server <- function(input, output, session) {
+  selected <- reactiveVal(rep(FALSE, nrow(parkrun_times)))
+  
+  observeEvent(input$plot_brush, {
+    brushed <- brushedPoints(parkrun_times, input$plot_brush, allRows = TRUE)$selected_
+    selected(brushed | selected())
+  })
+  observeEvent(input$plot_reset, {
+    selected(rep(FALSE, nrow(mtcars)))
+  })
+  
+  output$plot <- renderPlot({
+    parkrun_times$sel <- selected()
+    ggplot(parkrun_times, aes(date, time)) + 
+      geom_point(aes(colour = sel)) +
+      theme_classic()+
+      scale_colour_discrete(limits = c("TRUE", "FALSE"))
+  }, res = 96)
+}
+
+
+
+shinyApp(ui, server) 
 
 
 
