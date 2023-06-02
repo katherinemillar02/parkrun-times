ad4668dc1ee0ddd006dedfb5a39edea205fbcf1e
Katie Millar <ajh20fhu@uea.ac.uk>
2023-06-02 09:30:12 +0100

    trying to one two things in one app

--- ui/multipleshinyapp.R
+++ ui/multipleshinyapp.R
@@ -1,38 +1,12 @@
-# trying to get multiple things on one oage with fluid page?
+# trying to get multiple things on one page with fluid page?
 
+# loading the shiny package in
 library(shiny)
+library(ggplot2)
+library(DT)
 
-ui <- fluidPage(
-  tabsetPanel(
-    tabPanel("parkrun times", 
-             h2("parkrun times"),
-             DT::dataTableOutput("mytable")
-    ),
-    tabPanel("parkrun plots", 
-            h2("visualising parkruns"),
-            plotOutput("plot", brush = "plot_brush", dblclick = "plot_reset")
-           
-            
-            
-  
-             ),
-    tabPanel("Number of parkruns",
-             h2("comparing parkruns per person"))
-  )
-)
-
-shinyApp(ui, server)
-
-
-renderPlot({
-  parkrun_times$sel <- selected()
-  ggplot(parkrun_times, aes(date, time)) + 
-    geom_point(aes(colour = sel)) +
-    theme_classic()+
-    scale_colour_discrete(limits = c("TRUE", "FALSE"))
-}, res = 96)
-
-
+# creating a ui with fluid page
+# ui - controls layout? 
 ui <- fluidPage(
   tabsetPanel(
     tabPanel("parkrun times", 
@@ -48,7 +22,7 @@ ui <- fluidPage(
                     theme_classic()+
                     scale_colour_discrete(limits = c("TRUE", "FALSE"))
                 }, res = 96)
-                ),
+             ),
              plotOutput("plot", brush = "plot_brush", dblclick = "plot_reset")
              
              
@@ -56,9 +30,17 @@ ui <- fluidPage(
              
     ),
     tabPanel("Number of parkruns",
-             h2("comparing parkruns per person"))
+             h2("comparing parkruns per person")),
+    tabPanel("parkrun map",
+             h2("visualising map points")),
   )
 )
 
+
+
+
 shinyApp(ui, server)
 
+
+
+#58cm 
