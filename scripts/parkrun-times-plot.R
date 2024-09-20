46ec784dae20a78ad202684e1c5bcb187504566c
Katie Millar <ajh20fhu@uea.ac.uk>
2024-09-20 16:30:17 +0100

    Getting rid of old code

--- scripts/parkrun-times-plot.R
+++ scripts/parkrun-times-plot.R
@@ -5,39 +5,14 @@ library(RColorBrewer)
 library(plotly)
 
 # reading the data in
-parkrun_times <- read_excel("data/parkrun-times-2.xlsx", sheet = "Sheet1")
-
-
-
-# code for line and point plot 
-parkrun_plot_line <- parkrun_times %>% 
-ggplot(aes(x = date, y = time, color = parkrun)) + 
-  geom_point() +
-  scale_color_manual(values = brewer.pal(n = 5, name = "Set1")) +
-  geom_line() +
-  scale_size_continuous(range = c(2, 10)) +
-  labs(x = "Date", y = "Time", color = "parkrun", size = "Time") +
-  theme_classic() + 
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-
-# code for point plot 
-parkrun_plot_point <- parkrun_times %>% 
-  ggplot(aes(x = date, y = time, colour = parkrun)) + 
-  geom_point()+
-  scale_colour_manual(values = brewer.pal(n = 5, name = "Set1")) +
-  scale_size_continuous(range = c(2, 10)) +
-  labs(x = "Date", y = "Time", color = "parkrun", size = "Time") +
-  theme_classic() + 
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
+parkrun_times <- read_excel("data/parkrunstats.xlsx", sheet = "Sheet1")
 
 
 colours <- c("red", "blue")
 
 # plotly plot
 parkrun_plotly_point <- plot_ly(data = parkrun_times, x = ~date, y = ~time,
-                                color = ~parkrun, colours = colours, size = ~time) %>%
+                                color = ~parkrun, colours = colours) %>%
 
 
   add_markers() %>%
